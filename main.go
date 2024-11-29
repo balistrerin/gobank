6289073bfb059c48e8f624a534d8a7f875c79fcf
Nick Balistreri <balistrerin@gmail.com>
2024-11-28 23:07:21 -0800

    Your descriptive commit message

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", store)
+
+	server := NewAPIServer(":3000", store)
 	server.Run()
-	fmt.Println("Yea Buddy!")
+
 }
